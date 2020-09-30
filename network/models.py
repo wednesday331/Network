ddf2d622a6aa7d309d71586a9c2b3dfbdf778d09
wednesday331 <65116875+wednesday331@users.noreply.github.com>
2020-09-30 15:37:01 -0400

    PEP8 friendly formatting

--- network/models.py
+++ network/models.py
@@ -1,29 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Registered classes for the Network Project"""
+
 # Register your models here.
 from datetime import datetime
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# User class
 class User(AbstractUser):
+    """Abstract class for users to be used in views.py"""
     pass
 
+# Post class
 class Post(models.Model):
+    """Post components for each post in the network."""
     content = models.CharField(max_length=255)
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='author')
+    user = models.ForeignKey('User',
+                             on_delete=models.CASCADE,
+                             related_name='author')
     date = models.DateTimeField(default=datetime.now())
-    thumbsuped = models.ManyToManyField('User', default=None, blank=True, related_name='postlikesno')
+    thumbsuped = models.ManyToManyField('User',
+                                        default=None,
+                                        blank=True,
+                                        related_name='postlikesno')
 
     @property
-    def likesnumber(self):
+    def likes_number(self):
         return self.thumbsuped.all().count()
 
+# Profile Class    
 class Profile(models.Model):
-    target = models.ForeignKey('User', on_delete=models.CASCADE, related_name='followers')
-    follower = models.ForeignKey('User', on_delete=models.CASCADE, related_name='targets')
-
+    """Profile information components."""
+    target = models.ForeignKey('User',
+                               on_delete=models.CASCADE,
+                               related_name='followers')
+    follower = models.ForeignKey('User',
+                                 on_delete=models.CASCADE,
+                                 related_name='targets')
+# Like/Dislike Class
 class Like(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    """Class for liking/disliking posts."""
+    user = models.ForeignKey('User',
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey('Post',
+                             on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.post)
